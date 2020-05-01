4fd317f184a0a1d0bc1d1337693d59e2349ecd17
lidexin <lidexin929@163.com>
2020-05-01 17:14:54 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.itheima.git;
+
+/**
+ * @author ldx
+ * @create 2020-05-01 16:56
+ */
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
